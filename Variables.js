def9ed00c1ed32b5c05ea016696a58bc7ba46c3d
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-05-23 22:46:02 +0100

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,44 @@
+const location = "New York City";
+let latitude = 40.7;
+let isNorthernHemisphere = true;
+
+console.log(location);
+console.log(latitude);
+console.log(inNorthernHemisphere);
+
+const entree = "Enchiladas";
+const price = 12;
+
+console.log(entree);
+console.log(price);
+
+entree = "Tacos";
+
+let changeMe = true;
+
+changeMe = false;
+console.log(changeMe);
+
+let notDefined;
+let valueless;
+console.log(valueless);
+
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+
+molecule = 16 + molecule;
+
+
+particle = particle * 6.02;
+
+
+assay = assay + 1;
+
+let favoriteAnimal = "Penguin";
+console.log("My favorite animal:" + favoriteAnimal);
+
+let myName = "Suoarik";
+let myCity = "London";
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
