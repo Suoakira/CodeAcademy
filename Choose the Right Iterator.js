5b37118837fdebda5f456e0f1c1a5712f2b6c0e6
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-05-25 17:11:19 +0100

    Add files via upload

---
+++ Choose the Right Iterator.js
@@ -0,0 +1,105 @@
+// Introduction to Iterators
+
+let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+
+artists.forEach(function(artist) {
+  console.log(artist + ' is one of my favorite artists.'); // forEach part of the array it adds another line
+});
+
+let numbers = [1, 2, 3, 4, 5];
+
+let squareNumbers = numbers.map(function(number) { // maps numbers in this case squares them
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+let onlyNumbers = things.filter(function(thing) { // filters through thigns specified in the function
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
+
+// .forEach 
+
+let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+
+// .map
+
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+
+let secretMessage = animals.map(animal => animal.charAt(0));
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+
+let smallNumbers = bigNumbers.map(num => num/100);
+
+// .filter
+
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+let smallNumbers = randomNumbers.filter(num => num < 250); // can be written .filer(function(num) { return num <250;
+
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+
+// Refactor the code above using arrow function syntax
+
+// Iterator Documentation
+
+/* 
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+*
+*/
+
+let words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(function(word) {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+let interestingWords = words.filter(word => word.length > 5);
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every(word =>  word.length > 5      ));
+
+// chooseing the right Iterator
+
+let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+
+let nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+let longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a new array
+let smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every(num => num < 0);
+
+
+
