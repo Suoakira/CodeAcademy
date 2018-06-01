2201a543e05c44d14cf85eb7897cc408a75fa799
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-06-01 16:25:18 +0100

    Add files via upload

--- Modules.js
+++ Modules.js
@@ -80,3 +80,88 @@ displayFuelCapacity();
 
 // Named Exports
 
+let availableAirplanes = [{
+    name: 'AeroJet',
+    fuelCapacity: 800,
+    availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+},
+{
+    name: 'SkyJet',
+    fuelCapacity: 500,
+    availableStaff: ['pilots', 'flightAttendants']
+}];
+
+let flightRequirements = {
+    requiredStaff: 4,
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+    if (availableStaff.length >= requiredStaff) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export { availableAirplanes, flightRequirements, meetsStaffRequirements };
+
+    // Named Imports
+
+import { availableAirplanes, flightRequirements, meetsStaffRequirements } from './airplane';
+
+function displayFuelCapacity() {
+    availableAirplanes.forEach(function (element) {
+        console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+    });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+    availableAirplanes.forEach(function (element) {
+        console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff));
+    });
+}
+
+displayStaffStatus();
+
+// Export Named Exports
+
+export let availableAirplanes = [
+    {
+        name: 'AeroJet',
+        fuelCapacity: 800,
+        availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+        maxSpeed: 1200,
+        minSpeed: 300
+    },
+    {
+        name: 'SkyJet',
+        fuelCapacity: 500,
+        availableStaff: ['pilots', 'flightAttendants'],
+        maxSpeed: 800,
+        minSpeed: 200
+    }
+];
+
+export let flightRequirements = { // exports soon as it has run so can be used straight away rather than at the bottom where you would have to wait for  the whole script to play out!
+    requiredStaff: 4,
+    requiredSpeedRange: 700
+};
+
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+    if (availableStaff.length >= requiredStaff) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+    let range = maxSpeed - minSpeed;
+    if (range > requiredSpeedRange) {
+        return true;
+    } else {
+        return false;
+    }
+};
