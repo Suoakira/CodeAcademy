9a2462240ad4b8594632b6513c9fa3c462ac9b03
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-06-04 18:53:25 +0100

    Add files via upload

--- Introduction to Requests.js
+++ Introduction to Requests.js
@@ -54,4 +54,19 @@ jsonButton.addEventListener('click', generateJson);
 
 // part 2
 
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint'
+
+xhr.responseType = 'json';
+
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        return xhr.response;
+    }
+}
+
+xhr.open('GET', url)
+xhr.send();
+
+
 // part 3
