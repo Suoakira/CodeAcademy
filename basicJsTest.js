4fc870745dc8835bcd1744a7218dae7350fc3cf8
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-08-06 22:29:52 +0100

    Add files via upload

---
+++ basicJsTest.js
@@ -0,0 +1,48 @@
+
+// looping a triangle
+
+let hash = "";
+
+function spam() {
+  for (var i = 0; i < 8; i++) {
+    hash = hash + "#";
+    console.log(hash);
+  }
+ }
+
+spam();
+
+//
+
+// better answer =
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+// Fizzbuzz
+
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
+
+// chessbored
+
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
