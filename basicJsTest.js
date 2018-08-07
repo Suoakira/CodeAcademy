bc8e56bedd42a48dcf41a29d8809dcee461ea91d
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-08-07 13:46:25 +0100

    Add files via upload

--- basicJsTest.js
+++ basicJsTest.js
@@ -1,3 +1,7 @@
+/*
+* These are exercise's i am doing from the eloquent javascript book
+*/
+
 
 // looping a triangle
 
@@ -46,3 +50,69 @@ for (let y = 0; y < size; y++) {
 }
 
 console.log(board);
+
+// To get a minimum between two numbers
+
+function calcul(a, b) {
+  if (a < b) {
+    console.log(a);
+  } else {
+    console.log(b);
+  } return;
+}
+  
+calcul(1, 10)
+
+// recursion statements
+ function isEven(num) {
+ 	if (num % 2 == 0) {
+ 		console.log("Yes im Even");
+ 	} else {
+ 		console.log("Once you eliminate the impossible, whatever remains,no matter how improbable, must be the truth, Yes it must be Odd")
+
+ 	}
+ }
+
+
+// basic boo leans 
+
+function isEven(num) {
+  if (num % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isCapitalB(str) {
+	for (let count = 0; count < str.length; count++) {
+		if (str[i] === "B") {
+			console.log("I Found it!")
+		}
+	
+
+	}
+}
+
+// counting beans
+
+function countChar(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
+
