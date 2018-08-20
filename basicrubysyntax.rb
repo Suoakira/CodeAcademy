df4508006760d3f4c58f85e701ffec87ac653905
Stephen <37964234+Suoakira@users.noreply.github.com>
2018-08-20 18:01:24 +0100

    Add files via upload

---
+++ basicrubysyntax.rb
@@ -0,0 +1,13 @@
+print "Whats your first name?"
+first_name = gets.chomp.capitalize!
+
+print "What is your last Name"
+last_name = gets.chomp.capitalize!
+
+print "What city are you from?"
+city = gets.chomp.capitalize!
+
+print "what state are you from?"
+state= gets.chomp.upcase!
+
+puts "#{first_name} #{last_name} #{city} #{state}"
